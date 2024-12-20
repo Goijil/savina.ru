fd843b69400cf86b83befd2aa9921c3552507e8c
Goijil <shishagin.k@gmail.com>
2024-12-20 09:27:55 +0300

    fix: deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/savina.ru/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: "index.html",
+    },
+  },
 });
