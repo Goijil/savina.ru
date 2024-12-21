f54a4d583e181c1ac9e4c5670645c9a6ed4ae0c7
Goijil <shishagin.k@gmail.com>
2024-12-21 10:47:25 +0300

    remove:yml + add:gh-pages + correct:(vite.config.js + package.json) for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
+  base: "/savina.ru",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
 });
